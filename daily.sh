a0c6ecbb4e6d9f7ac8ea429ed3548d58f7e5d88a
Peter <peter@exinto.de>
2015-05-09 00:20:32 +0200

    Make git quiet.

--- daily.sh
+++ daily.sh
@@ -3,4 +3,4 @@
 # Executed daily by an external cron job, will update the repository and the
 # user-crontab.
 
-git fetch && git reset --hard origin/master && crontab ./crontab
+git fetch && git reset --hard origin/master >/dev/null && crontab ./crontab
